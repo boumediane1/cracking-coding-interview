887b2bb6c82d329daaf47c2827e8455ea71e683d
Otmane Boumediane <boumediane.otmane@gmail.com>
2025-10-09 22:38:06 +0100

    feat: solve 1.2 - check permutation

---
+++ check_permutation.kt
@@ -0,0 +1,21 @@
+fun checkPermutation(a: String, b: String): Boolean {
+    if (a.length != b.length) {
+        return false
+    }
+
+    val m1 = mutableMapOf<Char, Int>()
+    val m2 = mutableMapOf<Char, Int>()
+
+    for (i in 0 until a.length) {
+        m1[a[i]] = m1.getOrDefault(a[i], 0) + 1
+        m2[b[i]] = m2.getOrDefault(b[i], 0) + 1
+    }
+
+    for (c in 'a' until 'z') {
+        if (m1[c] != m2[c]) {
+            return false
+        }
+    }
+
+    return true
+}
