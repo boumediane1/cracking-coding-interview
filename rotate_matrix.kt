82c95fcd1d647f7d2894c7b15ba1e03fc55ef3fa
Otmane Boumediane <boumediane.otmane@gmail.com>
2025-10-18 16:25:32 +0100

    refactor: rotate matrix in place

--- rotate_matrix.kt
+++ rotate_matrix.kt
@@ -1,12 +1,23 @@
 fun rotateMatrix(matrix: Array<Array<Int>>): Array<Array<Int>> {
-    val result = Array(3) { Array(3) { 0 } }
-
-    for (i in 0 until matrix.size) {
-        for (j in 0 until matrix.size) {
-            result[j][matrix.size - 1 - i] = matrix[i][j]
+    for (i in 0 until matrix.size / 2) {
+        for (j in i until matrix.size - 1 - i) {
+            val temp = matrix[i][j]
+            matrix[i][j] = matrix[j][matrix.size - 1 - i]
+            matrix[j][matrix.size - 1 - i] = temp
         }
-        println()
+
+        for (j in i until matrix.size - 1 - i) {
+            val temp = matrix[i][j]
+            matrix[i][j] = matrix[matrix.size - 1 - i][matrix.size - 1 - j]
+            matrix[matrix.size - 1 - i][matrix.size - 1 - j] = temp
+        }
+
+        for (j in i until matrix.size - 1 - i) {
+            val temp = matrix[i][j]
+            matrix[i][j] = matrix[matrix.size - 1 - j][i]
+            matrix[matrix.size - 1 - j][i] = temp
+        }
     }
 
-    return result
+    return matrix
 }
