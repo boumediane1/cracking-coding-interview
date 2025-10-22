3081158e293b4a00a45eece4f9f579ae6fabde57
Otmane Boumediane <boumediane.otmane@gmail.com>
2025-10-22 20:44:01 +0100

    fix: check for unique ASCII characters and add more assertions

--- src/main/kotlin/IsUnique.kt
+++ src/main/kotlin/IsUnique.kt
@@ -1,8 +1,8 @@
 fun isUnique(s: String): Boolean {
-    val letters = Array(26) { 0 }
+    val letters = Array(256) { 0 }
 
-    for (c in s.lowercase()) {
-        val index = c.code - 'a'.code
+    for (c in s) {
+        val index = c.code
 
         letters[index]++
 
--- src/test/kotlin/IsUniqueTest.kt
+++ src/test/kotlin/IsUniqueTest.kt
@@ -7,8 +7,10 @@ class IsUniqueTest {
         assertEquals(true, isUnique("abcd"))
         assertEquals(true, isUnique("s4fad"))
         assertEquals(true, isUnique(""))
+        assertEquals(true, isUnique(CharArray(128) { it.toChar() }.joinToString("")))
 
         assertEquals(false, isUnique("23ds2"))
         assertEquals(false, isUnique("hb 627jh=j ()"))
+        assertEquals(false, isUnique(CharArray(129) { (it / 2).toChar() }.joinToString("")))
     }
 }
